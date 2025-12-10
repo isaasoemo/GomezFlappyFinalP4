80e4eb38d89a9576f4a1789157f1530bb488f87e
isaasoemo <731217@student.dusd.net>
2025-12-10 11:42:07 -0800

    Added sprites
    created anims and transitions added the game over texts need to finish scripting game over text

--- P4FlappyFInalGomez/Assets/Scripts/Bird.cs
+++ P4FlappyFInalGomez/Assets/Scripts/Bird.cs
@@ -9,12 +9,14 @@ public class Bird : MonoBehaviour
 
     private bool isDead = false;
     private Rigidbody2D rb2d;
+    private Animator anim;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D> ();
+        anim = GetComponent<Animator> ();
     }
 
     // Update is called once per frame
@@ -26,11 +28,14 @@ public class Bird : MonoBehaviour
             {
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upforce));
+                anim.SetTrigger("Flap");
             }
         }
     }
-    void OnCollision2D ()
+    void OnCollisionEnter2D ()
     {
         isDead = true;
+        anim.SetTrigger("Die");
+        GameControl.instance.BirdDied();
     }
 }
